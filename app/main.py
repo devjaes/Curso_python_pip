858e9ccf096c69df909ceeab91502a7d3b82a704
Jaesbyp <merajair17@gmail.com>
2022-11-23 14:59:24 -0500

    Add web server topics

--- app/main.py
+++ app/main.py
@@ -1,21 +1,30 @@
 import utils
 import read_csv
 import charts
+import pandas as pd
 
 
 def run():
+    
+
     data = read_csv.read_csv('./data.csv')
     country = input('Type country => ')
-
     result = utils.population_by_country(data, country)
     if len(result) > 0:
         name_country = country
         country = result[0]
         labels, values = utils.get_population(country)
-        charts.generate_bar_chart(name_country, labels, values)
-    labels, values = read_csv.get_world_percentages(data)
+    #labels, values = read_csv.get_world_percentages(data)
 
-    #charts.generate_pie_chart(name_country, labels, values)
+
+    df = pd.read_csv('data.csv')
+    df = df[df['Continent']== 'Africa']
+    countries = df['Country'].values
+    percentajes = df['World Population Percentage'].values
+
+    charts.generate_bar_chart(name_country, labels, values)
+    #charts.generate_pie_chart(name_country, countries, percentajes)
+
 
 
 if __name__ == '__main__':
